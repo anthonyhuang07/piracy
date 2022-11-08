932488438e384ada33642929af6274a1f76b3116
Anthony Huang <anthonyhuang07@icloud.com>
2022-11-07 20:09:52 -0500

    script

--- src/scripts/macvirus.js
+++ src/scripts/macvirus.js
@@ -1,7 +1,20 @@
 let elem = document.documentElement;
 
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+      end = new Date().getTime();
+   }
+}
+
+function popup(){
+    alert("IUHFWIURGHIUGHEUF")
+}
+
 document.body.onclick = function(){
     elem.requestFullscreen();
     elem.webkitRequestFullscreen();
-    elem.msRequestFullscreen();
+    wait(5000);
+    popup();
 }
